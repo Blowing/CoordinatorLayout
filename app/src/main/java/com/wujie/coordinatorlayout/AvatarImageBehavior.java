978da4c08da76b090e37391bb8c49efb05754e0c
Administrator <1072878103@qq.com>
2016-09-05 21:53:29 +0800

    提交代码

--- app/src/main/java/com/wujie/coordinatorlayout/AvatarImageBehavior.java
+++ app/src/main/java/com/wujie/coordinatorlayout/AvatarImageBehavior.java
@@ -14,94 +14,119 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class AvatarImageBehavior extends CoordinatorLayout.Behavior<CircleImageView> {
 
     private final static float MIN_AVATAR_PERCENTAGE_SIZE = 0.3f;
-    private final static float EXTRA_FINAL_AVATAR_PADDING = 80;
+    private final static int EXTRA_FINAL_AVATAR_PADDING = 80;
 
-    private int mStartYPosition;
-    private int mFinalYPosition;
-    private int mStartHeight;
-    private int mFinalHeight;
-    private int mStartXPosition;
-    private int mFinalXPosition;
-    private float mStartToolbarPosition;
+    private int mStartYPosition; // 起始的Y轴位置
+    private int mFinalYPosition; // 结束的Y轴位置
+    private int mStartHeight; // 开始的图片高度
+    private int mFinalHeight; // 结束的图片高度
+    private int mStartXPosition; // 起始的X轴高度
+    private int mFinalXPosition; // 结束的X轴高度
+    private float mStartToolbarPosition; // Toolbar的起始位置
 
     private final Context mContext;
     private float mAvatarMaxSize;
 
-
-    public AvatarImageBehavior(Context context, AttributeSet attributeSet) {
+    public AvatarImageBehavior(Context context, AttributeSet attrs) {
         mContext = context;
         init();
     }
 
     private void init() {
-        bingDimensions();
+        bindDimensions();
     }
 
-    private void bingDimensions() {
+    private void bindDimensions() {
         mAvatarMaxSize = mContext.getResources().getDimension(R.dimen.image_width);
-
     }
+
 
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, CircleImageView child, View dependency) {
+        // 依赖Toolbar控件
         return dependency instanceof Toolbar;
     }
 
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, CircleImageView child, View dependency) {
+
+        // 初始化属性
         shouldInitProperties(child, dependency);
-        final int maxScroolDistance = (int) (mStartToolbarPosition - getStatusBarHeight());
 
-        float expendedPercentageFactor = dependency.getY() / maxScroolDistance;
+        // 最大滑动距离: 起始位置-状态栏高度
+        final int maxScrollDistance = (int) (mStartToolbarPosition - getStatusBarHeight());
 
+        // 滑动的百分比
+        float expandedPercentageFactor = dependency.getY() / maxScrollDistance;
+
+        // Y轴距离
         float distanceYToSubtract = ((mStartYPosition - mFinalYPosition)
-                * (1f - expendedPercentageFactor)) + (child.getHeight() / 2);
+                * (1f - expandedPercentageFactor)) + (child.getHeight() / 2);
 
+        // X轴距离
         float distanceXToSubtract = ((mStartXPosition - mFinalXPosition)
-                * (1f - expendedPercentageFactor)) + (child.getWidth() / 2);
+                * (1f - expandedPercentageFactor)) + (child.getWidth() / 2);
 
-        float heightToSubtract = ((mStartHeight - mFinalHeight) * (1f - expendedPercentageFactor));
+        // 高度减小
+        float heightToSubtract = ((mStartHeight - mFinalHeight) * (1f - expandedPercentageFactor));
 
-        child.setX(mStartYPosition - distanceYToSubtract);
-        child.setY(mStartXPosition - distanceXToSubtract);
+        // 图片位置
+        child.setY(mStartYPosition - distanceYToSubtract);
+        child.setX(mStartXPosition - distanceXToSubtract);
 
-        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams)child.getLayoutParams();
+        // 图片大小
+        CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
         lp.width = (int) (mStartHeight - heightToSubtract);
         lp.height = (int) (mStartHeight - heightToSubtract);
         child.setLayoutParams(lp);
 
-        return super.onDependentViewChanged(parent, child, dependency);
+        return true;
     }
 
+    /**
+     * 初始化动画值
+     *
+     * @param child      图片控件
+     * @param dependency ToolBar
+     */
     private void shouldInitProperties(CircleImageView child, View dependency) {
-        if(mStartYPosition == 0) {
-            mStartHeight = (int) (child.getY()) + (child.getHeight() / 2);
-        }
-        if(mFinalYPosition == 0) {
+
+        // 图片控件中心
+        if (mStartYPosition == 0)
+            mStartYPosition = (int) (child.getY() + (child.getHeight() / 2));
+
+        // Toolbar中心
+        if (mFinalYPosition == 0)
             mFinalYPosition = (dependency.getHeight() / 2);
-        }
-        if(mStartHeight == 0) {
+
+        // 图片高度
+        if (mStartHeight == 0)
             mStartHeight = child.getHeight();
-        }
-        if(mFinalHeight == 0) {
+
+        // Toolbar缩略图高度
+        if (mFinalHeight == 0)
             mFinalHeight = mContext.getResources().getDimensionPixelOffset(R.dimen.image_final_width);
-        }
-        if(mStartXPosition == 0) {
-            mStartXPosition = (int) (child.getX() + (child.getWidth()) / 2);
-        }
-        if(mFinalXPosition == 0) {
+
+        // 图片控件水平中心
+        if (mStartXPosition == 0)
+            mStartXPosition = (int) (child.getX() + (child.getWidth() / 2));
+
+        // 边缘+缩略图宽度的一半
+        if (mFinalXPosition == 0)
             mFinalXPosition = mContext.getResources().getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + (mFinalHeight / 2);
-        }
-        if(mStartToolbarPosition == 0) {
+
+        // Toolbar的起始位置
+        if (mStartToolbarPosition == 0)
             mStartToolbarPosition = dependency.getY() + (dependency.getHeight() / 2);
-        }
     }
 
-    public float getStatusBarHeight() {
+    // 获取状态栏高度
+    public int getStatusBarHeight() {
         int result = 0;
         int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
+
         if (resourceId > 0) {
-            result = mContext.getResources().getDimensionPixelOffset(resourceId);
+            result = mContext.getResources().getDimensionPixelSize(resourceId);
         }
         return result;
     }
--- app/src/main/java/com/wujie/coordinatorlayout/MainActivity.java
+++ app/src/main/java/com/wujie/coordinatorlayout/MainActivity.java
@@ -1,13 +1,120 @@
 package com.wujie.coordinatorlayout;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR = 0.9f;
+    private static final float PERCENTAGE_TO_HIDE_TITLE_DETAILS = 0.3f;
+
+    private static final int ALPHA_ANIMATIONS_DURATION = 200;
+
+    private boolean mIsTheTitleVisible = false;
+    private boolean mIsTheTitleContainerVisible = true;
+
+    @BindView(R.id.main_iv_placeholder)
+    ImageView mIvPlaceholder;
+
+    @BindView(R.id.main_ll_title_container)
+    LinearLayout mLltelContainer;
+
+    @BindView(R.id.main_fl_title)
+    FrameLayout mFlTlTitleContainer;
+
+    @BindView(R.id.main_abl_app_bar)
+    AppBarLayout mAblAppbar;
+
+    @BindView(R.id.main_tv_toolbar_title)
+    TextView mTvToolbarTitle;
+
+    @BindView(R.id.main_tb_toolbar)
+    Toolbar mTbToobar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mTbToobar.setTitle("");
+        
+        mAblAppbar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                int maxScroll = appBarLayout.getTotalScrollRange();
+                float percentage = (float) Math.abs(verticalOffset) /(float) maxScroll;
+                handleAlphaOnTitle(percentage);
+                hadleToolbarTitleVisibility(percentage);
+            }
+        });
+        
+        initParallaxValues();
+    }
+
+    private void initParallaxValues() {
+
+        CollapsingToolbarLayout.LayoutParams petDetailsLp =
+                (CollapsingToolbarLayout.LayoutParams) mIvPlaceholder.getLayoutParams();
+
+        CollapsingToolbarLayout.LayoutParams petBackgroundLp =
+                (CollapsingToolbarLayout.LayoutParams) mFlTlTitleContainer.getLayoutParams();
+
+        petDetailsLp.setParallaxMultiplier(0.9f);
+        petBackgroundLp.setParallaxMultiplier(0.3f);
+
+        mIvPlaceholder.setLayoutParams(petDetailsLp);
+        mFlTlTitleContainer.setLayoutParams(petBackgroundLp);
+    }
+
+    private void hadleToolbarTitleVisibility(float percentage) {
+        if(percentage >= PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR) {
+            if(!mIsTheTitleVisible) {
+                startAlphaAnimation(mTvToolbarTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = true;
+            }
+        } else {
+            if(mIsTheTitleVisible) {
+                startAlphaAnimation(mTvToolbarTitle, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleVisible = false;
+            }
+        }
+    }
+
+    private void startAlphaAnimation(View v, int alphaAnimationsDuration, int visible) {
+        AlphaAnimation alphaAnimation = (visible == View.VISIBLE)
+                ? new AlphaAnimation(0f, 1f)
+                : new AlphaAnimation(1f, 0f);
+        alphaAnimation.setDuration(alphaAnimationsDuration);
+        alphaAnimation.setFillAfter(true);
+        v.startAnimation(alphaAnimation);
+
+    }
+
+    private void handleAlphaOnTitle(float percentage) {
+        if (percentage >= PERCENTAGE_TO_HIDE_TITLE_DETAILS) {
+            if (mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mLltelContainer, ALPHA_ANIMATIONS_DURATION, View.INVISIBLE);
+                mIsTheTitleContainerVisible = false;
+            }
+        } else {
+            if (!mIsTheTitleContainerVisible) {
+                startAlphaAnimation(mLltelContainer, ALPHA_ANIMATIONS_DURATION, View.VISIBLE);
+                mIsTheTitleContainerVisible = true;
+            }
+        }
     }
 }
